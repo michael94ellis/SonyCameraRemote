5ed48700c9fa6981b05241ec92ebfed9ea771bf4
Michael Ellis <michael94ellis@gmail.com>
2020-11-22 11:11:18 -0500

    Initial Commit

---
+++ SonyCameraRemote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SonyCameraRemote
+//
+//  Created by Michael Ellis on 11/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
